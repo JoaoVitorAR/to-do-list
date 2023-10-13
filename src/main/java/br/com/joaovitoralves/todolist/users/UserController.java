78177b4cd4f1fa8e7cf49de347fe3c74711b4d90
JoaoVitorAR <jvalves1998@outlook.com.br>
2023-10-13 11:08:15 -0300

    atualização mvn

--- src/main/java/br/com/joaovitoralves/todolist/users/UserController.java
+++ src/main/java/br/com/joaovitoralves/todolist/users/UserController.java
@@ -30,7 +30,6 @@ public class UserController{
         userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
-        
         return ResponseEntity.status(HttpStatus.OK).body(userCreated);
     }
 }
